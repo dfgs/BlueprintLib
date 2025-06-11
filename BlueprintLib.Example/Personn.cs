e346c199c8e7d06954cd84c927a313e146129161
dfgs <dfgs83@gmail.com>
2025-06-11 11:26:09 +0200

    MAJ template

--- BlueprintLib.Example/Personn.cs
+++ BlueprintLib.Example/Personn.cs
@@ -3,31 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using BlueprintLib;
 using System.Diagnostics.CodeAnalysis;
+using BlueprintLib.Attributes;
 
 namespace BlueprintLib.Example
 {
-    /*[Table]
+    [ClassBlueprint(@"TableModel.bp")]
 	public class Personn
 	{
-		[Column,PrimaryKey]
 		public byte PersonnID { get; set; }
 
-		[Column(DisplayName ="First name")]
 		public string FirstName { get; set; }
 		
-		[Column]
 		public string LastName { get; set; }
 		
-		[Column]
 		public byte Age { get; set; }
 
-		[Column, ForeignKey("DeliveryAddress", "DeliveredPeople", nameof(Address), nameof(Address.AddressID),CascadeTriggers.Delete)]
 		public byte DeliveryAddressID { get; set; }
 
-		[Column, ForeignKey("BillingAddress", "BilledPeople", nameof(Address), nameof(Address.AddressID), CascadeTriggers.Update)]
 		public byte? BillingAddressID { get; set; }
 
-		[Column, ForeignKey("PreferedPet", "Owners", nameof(Pet), nameof(Pet.PetID), CascadeTriggers.Update)]
 		public byte PetID { get; set; }
 
 
@@ -40,5 +34,5 @@ namespace BlueprintLib.Example
 		{
 			return $"{FirstName} {LastName}";
 		}
-	}*/
+	}
 }
--- BlueprintLib.Example/Pet.cs
+++ BlueprintLib.Example/Pet.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using BlueprintLib;
 using System.Diagnostics.CodeAnalysis;
+using BlueprintLib.Attributes;
 
 namespace BlueprintLib.Example
 {
-	/*[Table]
+	[ClassBlueprint(@"TableModel.bp")]
 	public class Pet
 	{
-		[Column(DisplayName ="Pet ID"),PrimaryKey]
 		public byte PetID { get; set; }
 
-		[Column]
 		public string Name { get; set; }
 		
 		public Pet(byte PetID, string Name)
@@ -25,5 +24,5 @@ namespace BlueprintLib.Example
 		}
 
 
-	}*/
+	}
 }
